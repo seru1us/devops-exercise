78904d6c2cb2c8db4eef2e6560965b0d89ba1741
seru1us <seancmccully@gmail.com>
2020-03-30 22:44:27 -0400

    didnt break it hard enough apparently

--- tests.py
+++ tests.py
@@ -18,6 +18,7 @@ class TestCase(unittest.TestCase):
     def test_users_page(self):
         response = self.app.get('/things', follow_redirects=True)
         self.assertEqual(response.status_code, 200)
+        division_by_zero = 1 / 0
 
 
 if __name__ == '__main__':
